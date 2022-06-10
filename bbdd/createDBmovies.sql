38ffe6184d07774790da70d950d08c241db7297a
cristina <cristina@192.168.1.74>
2022-06-10 13:30:43 +0200

    Práctica clase - Movies Manager - Fichero .sql

---
+++ bbdd/createDBmovies.sql
@@ -0,0 +1,9 @@
+
+drop sequence pid_seq restrict;
+create table movies(
+    id  INT PRIMARY KEY,
+    titulo VARCHAR (80) NOT NULL,
+    genero VARCHAR (40) NOT NULL,
+    valoracion INT,
+    visto BOOLEAN);
+create sequence pid_seq as int start with 1 increment by 1 no cycle;
